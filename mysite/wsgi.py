d39936502840f48f9d8401beb96863c31c73d3e7
normandantzig1 <normandantzig1@gmail.com>
2018-01-26 01:02:45 -0700

    Updated project to use WhiteNoise.

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,11 +10,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-application = Cling(get_wsgi_application())
-
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
